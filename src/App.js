a27d4ca63a719a86f6d95e0df1388416f3ba8853
Maximilian Ranger <maximilian@mail.rangerhome.homeip.net>
2021-12-11 14:07:40 +0100

    initial commit -> create base draft

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+//import titleImg from './Resources/images/title_img.jpg';
+import portraitImg from './Resources/images/portrait_img.png';
 
 function App() {
-  return (
+  return(
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <img src={portraitImg} className="App-img" alt="me" />
+        </div>
+        <div> 
+          <div className="App-title">
+            <h4>Hello there!</h4>
+            <h1>I'm Maximilian Ranger</h1>
+            <h2>software engineer</h2>
+          </div>
+          <div>
+            <p
+              className="App-link"
+              // link to tag #about-me
+            >
+              About me
+            </p>
+          </div>
+        </div>
       </header>
     </div>
   );
