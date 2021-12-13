7e8a6540dc87c0b3e023a9f994c1fb5ab7f3848d
Maximilian Ranger <maximilian@mail.rangerhome.homeip.net>
2021-12-13 11:43:14 +0100

    bug fix and ready for deployment

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+//import resume from '../public/maximilian_ranger_resume.pdf';
 
 class About extends Component {
   render() {
@@ -39,7 +40,7 @@ class About extends Component {
               </div>
               <div className="columns download">
                 <p>
-                  <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
+                  <a href={resumeDownload} className="button" target="_blank"><i className="fa fa-download"></i>Download Resume</a>
                 </p>
               </div>
             </div>
