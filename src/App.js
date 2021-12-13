00fd8aef79955313a5b0e16e18e492595d98f352
Maximilian Ranger <maximilian@mail.rangerhome.homeip.net>
2021-12-13 20:29:01 +0100

    try deploy on github pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
 import './App.css';
+import resumeData from './resumeData';
 import About from './Components/About';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
@@ -8,40 +9,39 @@ import Resume from './Components/Resume';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      resumeData: {}
-    };
+  //constructor(props) {
+  //  super(props);
+  //  this.state = {
+  //    resumeData: {}
+  //  };
+  //}
 
-  }
+  //getResumeData() {
+  //  $.ajax({
+  //    url: '/resumeData.json',
+  //    dataType: 'json',
+  //    cache: false,
+  //    success: function(data) {
+  //      this.setState({resumeData: data});
+  //    }.bind(this),
+  //    error: function(xhr, status, err) {
+  //      console.log(err);
+  //      alert(err);
+  //    }
+  //  });
+  //}
 
-  getResumeData() {
-    $.ajax({
-      url: '/resumeData.json',
-      dataType: 'json',
-      cache: false,
-      success: function(data) {
-        this.setState({resumeData: data});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.log(err);
-        alert(err);
-      }
-    });
-  }
-
-  componentDidMount() {
-    this.getResumeData();
-  }
+  //componentDidMount() {
+  //  this.getResumeData();
+  //}
 
   render() {
     return (
       <div className="App">
-        <Header data={this.state.resumeData.main} />
-        <About data={this.state.resumeData.main} />
-        <Resume data={this.state.resumeData.resume} />
-        <Footer data={this.state.resumeData.main} />
+        <Header data={resumeData.main} />
+        <About data={resumeData.main} />
+        <Resume data={resumeData.resume} />
+        <Footer data={resumeData.main} />
       </div>
     );
   }
